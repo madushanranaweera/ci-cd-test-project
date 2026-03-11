805363b44aa00ee75b7610fc46de693f7e22a7dc
madushan <madushan.r@novaneura.ai>
2026-03-11 07:52:36 +0530

    Add Spring Boot project

---
+++ src/main/java/com/test/cicdtestproject/CICdTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.cicdtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CICdTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CICdTestProjectApplication.class, args);
+    }
+
+}
